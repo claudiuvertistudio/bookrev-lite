2dd0c885359be0497be6a21c971c8afcb3861c6d
Hardeep Asrani <hardeepasrani@gmail.com>
2016-09-06 02:01:37 +0530

    !!! Load pingback only where needed
    Load pingback only when it's a singular page and when open. Mentioned #46

--- header.php
+++ header.php
@@ -18,7 +18,9 @@
 
     <link rel="profile" href="http://gmpg.org/xfn/11" />
 
+<?php if ( is_singular() && pings_open( get_queried_object() ) ) { ?>
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<?php } ?>
 
     
 
